cedfcbcd74af262cef02abf09ed7c4a9d6f421da
jcper <informaticajcper@gmail.com>
2015-08-01 00:05:52 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ var quizController=require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
 
 router.get('/quizes',                                                 quizController.index);
 router.get('/quizes/:quizId(\\d+)',                    quizController.show);
