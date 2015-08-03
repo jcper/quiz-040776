db751dd2339da179e0cdb6d157e6599db9fca4a4
jcper <informaticajcper@gmail.com>
2015-08-03 15:31:46 +0200

    p2p select

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -22,6 +22,7 @@ exports.edit = function(req, res) {
  exports.update = function(req, res) {
   req.quiz.pregunta = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema      = req.body.quiz.tema;
     
       req.quiz
       .validate()
@@ -31,7 +32,7 @@ exports.edit = function(req, res) {
          res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
          }else{
            req.quiz     //save: guarda campos pregunta y respuesta en DB
-           .save( {fields:  ["pregunta", "respuesta"]})
+           .save( {fields:  ["pregunta", "respuesta","tema"]})
            .then( function(){  res.redirect('/quizes');});
            }//Redireccion HTTP a Lista de preguntas (URL relativo)
        }
@@ -48,7 +49,7 @@ exports.edit = function(req, res) {
 //GET/quizes/new
 exports.new = function(req, res) {
 var quiz= models.Quiz.build( //crea objeto quiz
-   {pregunta:"Pregunta", respuesta: "Respuesta"}
+   {pregunta:"Pregunta", respuesta: "Respuesta", tema: "Tema"}
  );
  res.render('quizes/new', {quiz: quiz, errors: [ ]});
 };
@@ -65,7 +66,7 @@ exports.create = function(req, res) {
          res.render( 'quizes/new',  {quiz: quiz, errors: err.errors});
        }else{
       // guarda en DB los campos pregunta y respuesta de quiz
-   quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+   quiz.save({fields: ["pregunta", "respuesta","tema"]}).then(function(){
    res.redirect('/quizes')})//Redireccion HTTP (URL relativo) lista de preguntas
             }
          }
